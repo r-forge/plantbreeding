371104295f4ddec80836f91b2f69a439a2dced99
rosyara (Dr ) <rosyaraur@gmail.com>
2012-07-31 13:01:00 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plantbreeding@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -42,8 +42,68 @@ fclose($handle);
 echo $contents; } ?>
 
 <!-- end of project description -->
+<hr><b>Description:</b>
+</p>
+</br>
+<p>One of the main reasons for developing this package is that develop functions that are interest to plant breeders and geneticists.</p>
+</br>
+ Your feedback and suggestions are welcome.
+</br></br>
 
-<p> No content added. </p>
+<ul>
+  <ul>
+    <ul>
+       <li> The R-package <tt>plantbreeding</tt> collection of functions for analysis of data and visualization of results from Plant Breeding and Genetics experiments. <br>
+       <li> Functionalities </li> 
+      <ul>
+        <li> Data analysis </li>
+          <ul>  
+          <li> Planning and Analysis of Plant breeding specialized designs </li>
+		  <li> Estimation of variance components, heritability, and genetic correlation  </li>
+		  <li> Stability and genotype x environment interaction analysis </li>
+		  <li> Diversity and variability  analysis </li>
+		  <li>  Selection and Selection index </li>
+		  <li> Molecular breeding tools: Linkage, QTL mapping, association mapping </li>
+		  </ul>
+		<li> Data visualization </li>
+		  </ul>
+		 <li> Different functions to plot high quality graphics include both conventional and molecular breeding or genomewide graphics </li>. 
+          </ul>
+		  
+ <hr><b> Licensing and WARRANTY </b>		  
+            <li>The package comes with ABSOLUTELY NO WARRANTY; for details
+see <a href="http://www.gnu.org/copyleft/gpl.html">http://www.gnu.org/copyleft/gpl.html</a>
+(GPL). </li>
+ </ul>
+  </ul>
+<hr><b>Installation and Download:</b>
+<blockquote>
+  <blockquote>
+    <blockquote> 
+To install the latest development version of package <tt>plantbreeding</tt> from R-Forge (if you are running a recent R version), use<br>
+<br>
+<div style="text-align: center;"> <tt>install.packages("plantbreeding",repos="http://r-forge.r-project.org")</tt></div>
+<br>
+<blockquote>
+  <blockquote>
+    <blockquote> 
+<hr><b> R package dependencies </b>
+<blockquote>
+  <blockquote>
+    <blockquote>
+The plantbreeding depends upon lme4, nlme, kinship, lattice and ggplot2 R packages. These packages are found for free installation from Comprehensive R Archive Network (CRAN) for free. The packages need to be installed for plantbreeding to work for specific functions
+<br>
+<blockquote>
+  <blockquote>
+    <blockquote>
+
+<hr><b>Developer / Maintainer:</b>
+<blockquote>
+<p> The package is developed and maintained by Umesh Rosyara, Michigan State University, East Lansing.The bugs and suggestions can be mailed to the developer <rosyara@msu.edu>.</p>
+</br>  
+  </blockquote>
+
+
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
